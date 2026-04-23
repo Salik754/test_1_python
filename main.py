e6113f526bfccb3e7acfa1f57ee1feaf2d298668
Salik754 <salikahmadn505@deltalearns.ca>
2026-04-23 12:35:37 -0700

    modified docstring

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ This is a test for creating 3 robots, their assigned zones(" Downtown ", " Subur
 The name of the robots are Max for Downtown, Advil for Suburbs and Robby for Industrial.
 The purpose for the robots is to deliver the packages to their respective zones within 5-500km.
 We need to get the weather conditions from the user(Clear, Rain, Storm)
-If the weather is Storm, print "🚨 Deployment Unsafe!".
+If the distance is over 300 km, any robot carries more than 50 kg, or weather is Storm, print "🚨 Deployment Unsafe!".
 Otherwise, print a summary of robot names, zones, and cargo weights with the message: "🤖 Robots Ready for Delivery!".
+
 """
+
